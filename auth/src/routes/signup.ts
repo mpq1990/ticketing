695f72c24d22b50458ac31f3c1c9ef8d43da0008
Majid Qureshi <mpq1990@gmail.com>
2021-01-02 16:43:08 +0100

    feat: add password hashing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -21,10 +22,19 @@ router.post(
     }
 
     const { email, password } = req.body;
-    console.log('Create a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 

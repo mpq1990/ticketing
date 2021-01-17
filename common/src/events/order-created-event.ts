633aba7ae25106e1a9d7dacdb7e15892107a616d
Majid Qureshi <mpq1990@gmail.com>
2021-01-17 14:18:50 +0100

    feat: add order event definition

---
+++ common/src/events/order-created-event.ts
@@ -0,0 +1,16 @@
+import { Subjects } from './subjects';
+import { OrderState } from './types/order-status';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    state: OrderState;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}

9b237c306928f8ad249da3b30cdee895b0040906
Majid Qureshi <mpq1990@gmail.com>
2021-01-31 13:56:10 +0100

    chore: add expiration service skeleton

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,20 @@
+import Queue from 'bull';
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOS,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log(
+    'I want to publish an expiration:complete event for orderId',
+    job.data.orderId
+  );
+});
+
+export { expirationQueue };

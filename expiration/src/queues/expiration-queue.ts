5ff15a21a0f5e3e4ebad1048988e27d8a1fdf41b
Majid Qureshi <mpq1990@gmail.com>
2021-02-01 23:08:30 +0100

    fix: redis k8s svc

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,18 +1,20 @@
-import Queue from 'bull';
+import Queue from "bull";
 
 interface Payload {
   orderId: string;
 }
 
-const expirationQueue = new Queue<Payload>('order:expiration', {
+console.log(process.env.REDIS_HOST);
+
+const expirationQueue = new Queue<Payload>("order:expiration", {
   redis: {
-    host: process.env.REDIS_HOS,
+    host: process.env.REDIS_HOST,
   },
 });
 
 expirationQueue.process(async (job) => {
   console.log(
-    'I want to publish an expiration:complete event for orderId',
+    "I want to publish an expiration:complete event for orderId",
     job.data.orderId
   );
 });

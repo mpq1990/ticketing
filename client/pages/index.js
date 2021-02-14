9910de7702fb48af260af7a020cbad4dfe2d151f
Majid Qureshi <mpq1990@gmail.com>
2021-02-14 20:31:29 +0100

    feat: add new ticket, order

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser, tickets }) => {
@@ -6,6 +7,11 @@ const LandingPage = ({ currentUser, tickets }) => {
       <tr key={ticket.id}>
         <td>{ticket.title}</td>
         <td>{ticket.price}</td>
+        <td>
+          <Link href='/tickets/[ticketId]' as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
       </tr>
     );
   });
@@ -17,6 +23,7 @@ const LandingPage = ({ currentUser, tickets }) => {
           <tr>
             <th>Title</th>
             <th>Price</th>
+            <th>Link</th>
           </tr>
         </thead>
         <tbody>{ticketList}</tbody>

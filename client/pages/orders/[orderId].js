d55ed44082dd8bf2f9c23aa6aae286d74f92edc3
Majid Qureshi <mpq1990@gmail.com>
2021-02-14 21:59:40 +0100

    feat: add order show

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,35 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>;
+import { useEffect, useState } from 'react';
+import buildClient from '../../api/build-client';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>{timeLeft} seconds until order expires</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await buildClient(context, 'http://orders-svc:3000').get(
+    `/api/orders/${orderId}`
+  );
+  return { order: data };
 };
 
 export default OrderShow;

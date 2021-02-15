9fcf7b6ae39d51e4b58c01aba62d074b9fb154b6
Majid Qureshi <mpq1990@gmail.com>
2021-02-15 18:56:03 +0100

    chore: depl test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose, { mongo } from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting auth service...');
+  console.log('starting auth service');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,8 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
-  console.log('starting payment');
+  console.log('starting payment service');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
